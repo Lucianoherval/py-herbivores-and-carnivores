7b4c74e5d6b51c05d1dd75541b0e07190b209381
Luciano Herval <lucianoherval@gmail.com>
2026-07-17 09:53:49 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -12,3 +12,26 @@ class Animal:
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
+
+    def take_damage(self, damage: int) -> None:
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            Animal.die(self)
+
+    @staticmethod
+    def die(animal: "Animal") -> None:
+        Animal.alive.remove(victim)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+    
+
+class herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = True
