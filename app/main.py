8700f4f1602c38b8f0daff82a2ffa03963ba034c
Luciano Herval <lucianoherval@gmail.com>
2026-07-17 09:31:14 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+        
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
